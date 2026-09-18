3a650b79546fdda51b7d3c13e48f8be416d583ce
Ayushhcodes <157227440+Ayushhcodes@users.noreply.github.com>
2026-09-19 00:27:43 +0530

    Create authentication page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Auth from "./pages/Auth"
 
 const App = () => {
   return (
     <div>
-      Home Page
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/auth" element={<Auth/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      Home page hu
+    </div>
+  )
+}
+
+export default Home
